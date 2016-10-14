1fb63cbbb940134bd588396a65a93ba2a38a2852
AliM0 <alli0layan@gmail.com>
2016-10-14 09:17:38 +0100

    Add files via upload

---
+++ game_template_2/gameparser.py
@@ -0,0 +1,120 @@
+import string
+from string import punctuation
+
+# List of "unimportant" words (feel free to add more)
+skip_words = ['a', 'about', 'all', 'an', 'another', 'any', 'around', 'at',
+              'bad', 'beautiful', 'been', 'better', 'big', 'can', 'every', 'for',
+              'from', 'good', 'have', 'her', 'here', 'hers', 'his', 'how',
+              'i', 'if', 'in', 'into', 'is', 'it', 'its', 'large', 'later',
+              'like', 'little', 'main', 'me', 'mine', 'more', 'my', 'now',
+              'of', 'off', 'oh', 'on', 'please', 'small', 'some', 'soon',
+              'that', 'the', 'then', 'this', 'those', 'through', 'till', 'to',
+              'towards', 'until', 'us', 'want', 'we', 'what', 'when', 'why',
+              'wish', 'with', 'would']
+
+
+def filter_words(words, skip_words):
+    """This function takes a list of words and returns a copy of the list from
+    which all words provided in the list skip_words have been removed.
+    For example:
+
+    >>> filter_words(["help", "me", "please"], ["me", "please"])
+    ['help']
+
+    >>> filter_words(["go", "south"], skip_words)
+    ['go', 'south']
+
+    >>> filter_words(['how', 'about', 'i', 'go', 'through', 'that', 'little', 'passage', 'to', 'the', 'south'], skip_words)
+    ['go', 'passage', 'south']
+
+    """
+    """
+    x=0
+    y=0
+    new_words=[]
+    if words in skip_words:
+            return  words
+    else:
+        while x< len(words):
+            if not words[x] in skip_words:
+                new_words[y]= words[x]
+                x= x+1
+                y= y+1
+            else:
+                x=x+1
+            
+            return new_words
+    """
+    new_words=[]
+    for x in words:
+        if x not in skip_words:
+            new_words.append(x)
+        else:
+            pass
+    return new_words
+
+
+    
+def remove_punct(text):
+    """This function is used to remove all punctuation
+    marks from a string. Spaces do not count as punctuation and should
+    not be removed. The funcion takes a string and returns a new string
+    which does not contain any puctuation. For example:
+
+    >>> remove_punct("Hello, World!")
+    'Hello World'
+    >>> remove_punct("-- ...Hey! -- Yes?!...")
+    ' Hey  Yes'
+    >>> remove_punct(",go!So.?uTh")
+    'goSouTh'
+    """
+    no_punct = ""
+    for char in text:
+        if not (char in string.punctuation):
+            no_punct= no_punct + char
+    return no_punct
+"""
+    text = ''.join(char for char in text if char not in punctuation)
+    return text
+"""
+
+def normalise_input(user_input):
+    """This function removes all punctuation from the string and converts it to
+    lower case. It then splits the string into a list of words (also removing
+    any extra spaces between words) and further removes all "unimportant"
+    words from the list of words using the filter_words() function. The
+    resulting list of "important" words is returned. For example:
+
+    >>> normalise_input("  Go   south! ")
+    ['go', 'south']
+    >>> normalise_input("!!!  tAkE,.    LAmp!?! ")
+    ['take', 'lamp']
+    >>> normalise_input("HELP!!!!!!!")
+    ['help']
+    >>> normalise_input("Now, drop the sword please.")
+    ['drop', 'sword']
+    >>> normalise_input("Kill ~ tHe :-  gObLiN,. wiTH my SWORD!!!")
+    ['kill', 'goblin', 'sword']
+    >>> normalise_input("I would like to drop my laptop here.")
+    ['drop', 'laptop']
+    >>> normalise_input("I wish to take this large gem now!")
+    ['take', 'gem']
+    >>> normalise_input("How about I go through that little passage to the south...")
+    ['go', 'passage', 'south']
+
+    """
+    # Remove punctuation and convert to lower case
+
+    norm_words = remove_punct(user_input).lower()
+
+    norm_words = norm_words.split()
+
+    norm_words = filter_words(norm_words, skip_words)
+
+    return norm_words
+    #
+    # COMPLETE ME!
+    #
+    s= "lkjdl lkjdflkdj"
+    print("hello lkjdkj".split())
+    print(split(s)+s)
